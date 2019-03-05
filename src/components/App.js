c6aeb598bd94db2c36e031872cc4bab10977cc15
kamakuni <superradio86@gmail.com>
2019-03-06 00:46:13 +0900

    add Status component to App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Footer from './Footer'
+import Status from './Status'
 import AddTodo from '../containers/AddTodo'
 import VisibileTodoList from '../containers/VisibleTodoList'
 
 const App = () => (
     <div>
         <AddTodo />
+        <Status />
         <VisibileTodoList />
         <Footer />
     </div>
