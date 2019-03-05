43f5e36b760daf7262db891f3e07c2963eb870e6
kamakuni <superradio86@gmail.com>
2019-03-06 00:24:45 +0900

    add more action types

--- src/reducers/todos.js
+++ src/reducers/todos.js
@@ -1,14 +1,11 @@
 const todos = (state = [], action) => {
     switch (action.type) {
-        case 'ADD_TODO':
-            return [
-                ...state,
-                {
-                    id: action.id,
-                    text: action.text,
-                    completed: false
-                }
-            ];
+        case 'ADD_TODO_REQUEST':
+            return {...state, notice: 'sending data...'}
+        case 'ADD_TODO_SUCCESS':
+            return {...state, notice: 'sending data is completed'}
+        case 'ADD_TODO_ERROR':
+            return {...state, notice: 'adding todo is failed'}
         case 'TOGGLE_TODO':
             return state.map(
                 todo => (todo.id === action.id ? {...todo, completed: !todo.completed} : todo)
