c7c0eac6a7a200a8634a7da96576056395e07a0f
kamakuni <superradio86@gmail.com>
2019-03-06 00:37:21 +0900

    add components for Status

---
+++ src/components/Status.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import PropTypes from 'prop-types'
+
+let Status = ({ notice }) => {
+    if (!notice) {
+        return <div></div>
+    }
+    return (
+        <div>
+            {notice}
+        </div>
+    )
+}
+
+Status.propTypes = {
+    notice: PropTypes.string
+}
+
+const mapStateToProps = state => {
+    return {
+        notice: state.todos.otice
+    }
+}
+
+Status = connect(mapStateToProps)(Status)
+
+export default Status;
