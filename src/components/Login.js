f75a291bf7981999cd25f24d4e401f28ed284440
kamakuni <superradio86@gmail.com>
2019-03-12 00:12:11 +0900

    create a Login component

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,45 @@
 import React from 'react'
+import { isLoaded, isEmpty } from 'react-redux-firebase'
+import { loginWithGoogle, logout } from '../actions/auth'
 
-const Login = () => (
-    ''
+
+const Login = ({ auth, loginWithGoogle, logout }) => {
+    if (!isLoaded(auth)) {
+        return (<div>Loginig in...</div>)
+    }
+    if (isEmpty(auth)) {
+        return (
+            <button onClick="loginWithGoogle">Login with Google account</button>
+        )
+    }
+    return (
+        <div>
+            {auth.displayName}
+            <button onCLick="logout">Logout</button>
+        </div>
+    )
+}
+
+Login.propTypes = {
+    auth: PropTypes.object.isRequired,
+    loginWithGoogle: PropTypes.func.isRequired,
+    logout: PropTypes.func.isRequired
+}
+
+const mapStateToProps = state => (
+    { auth: state.firebase.auth }
 )
 
-export default Login
+const mapDispatchToProps = dispatch => {
+    return {
+        loginWithGoogle: () => dispatch(loginWithGoogle()),
+        logout: () => dispatch(logout())
+    }
+}
+
+Login = connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(Login)
+
+export default Login;
