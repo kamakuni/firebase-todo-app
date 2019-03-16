f9259539b82a032ab2ce08ba2cce3e4332b0da93
kamakuni <superradio86@gmail.com>
2019-03-16 16:59:27 +0900

    add elements for unauthentecated users

--- src/components/TodoApp.js
+++ src/components/TodoApp.js
@@ -4,13 +4,24 @@ import Status from './Status'
 import AddTodo from '../containers/AddTodo'
 import VisibileTodoList from '../containers/VisibleTodoList'
 
-const TodoApp = ({uid, authenticating, authenticates}) => (
-    <div>
-        <AddTodo />
-        <Status />
-        <VisibileTodoList />
-        <Footer />
-    </div>
-)
+const TodoApp = ({uid, authenticating, authenticated}) => {
+
+    if (authenticating) {
+        return <div>logging in</div>
+    }
+    if (!authenticated) {
+        return <div>Login to show your task lint.</div>
+    }
+    return (
+        <div>
+            <AddTodo />
+            <Status />
+            <VisibileTodoList />
+            <Footer />
+        </div>
+    )
+}
+
+
 
 export default TodoApp
